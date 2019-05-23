990e04ce441e2b73c962f3d4325f8c2d53f42432
dontsaymyname <michaelpinheiro00@gmail.com>
2019-05-23 16:04:27 -0300

    Verificar getCursoPorCodigo

---
+++ src/Matricula.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Matricula {
+
+    private Aluno aluno;
+    private Curso curso;
+    private Date dataDeMatricula;
+
+    public Matricula(Aluno aluno, Curso curso) {
+        this.aluno = aluno;
+        this.curso = curso;
+        this.dataDeMatricula = new Date();
+    }
+}
